9dec9636da88bc6bc0c8e3c058bb59f26eaa96e8
Elena Willen <lnamaso@hotmail.com>
2016-05-31 21:26:59 +0100

    implement addListVC (v2)

---
+++ iWish/AddListViewController.m
@@ -0,0 +1,84 @@
+//
+//  AddListViewController.m
+//  iWish
+//
+//  Created by Elena Maso Willen on 31/05/2016.
+//  Copyright © 2016 Elena. All rights reserved.
+//
+
+#import "AddListViewController.h"
+
+#import <CoreData/CoreData.h>
+#import "AppDelegate.h"
+
+#import "List.h"
+
+@interface AddListViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *listNameTextField;
+
+- (IBAction)doneButtonPressed:(id)sender;
+- (IBAction)cancelButtonPressed:(id)sender;
+
+@end
+
+@implementation AddListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - TextField Delegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    BOOL rc = NO;
+    if (![textField.text isEqualToString:@""]) {
+        [textField resignFirstResponder];
+        rc = YES;
+    }
+    return rc;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - Action handlers
+
+- (IBAction)doneButtonPressed:(id)sender {
+    
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    
+    if (![self.listNameTextField.text isEqualToString:@""]) {
+        List *aList = [NSEntityDescription insertNewObjectForEntityForName:@"List" inManagedObjectContext:appDelegate.managedObjectContext];
+        aList.name = self.listNameTextField.text;
+        
+//        anItem.image = [self saveImageToDisk];
+        
+        NSError *error = nil;
+        [appDelegate.managedObjectContext save:&error];
+        if (error) {
+            NSLog(@"Core Data could not save: %@", [error localizedDescription]);
+        }
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+    
+}
+
+- (IBAction)cancelButtonPressed:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
